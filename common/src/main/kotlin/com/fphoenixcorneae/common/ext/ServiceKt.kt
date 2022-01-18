b1da6c5c1013107043295b270a570cff328b0225
wangkz <wangkz@infore.com>
2022-01-18 14:01:37 +0800

    feat:ServiceKt.kt; rm:ServiceUtil.kt;

---
+++ common/src/main/kotlin/com/fphoenixcorneae/common/ext/ServiceKt.kt
@@ -0,0 +1,126 @@
+package com.fphoenixcorneae.common.ext
+
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
+import java.util.*
+
+/**
+ * Return all of the services are running.
+ *
+ * @return all of the services are running
+ */
+val allRunningServices: Set<String>?
+    get() {
+        val info = appContext.activityManager?.getRunningServices(Integer.MAX_VALUE)
+        val names = HashSet<String>()
+        if (info == null || info.size == 0) {
+            return null
+        }
+        for (aInfo in info) {
+            names.add(aInfo.service.className)
+        }
+        return names
+    }
+
+/**
+ * @desc：Service
+ * @date：2021/11/26 16:33
+ */
+
+/**
+ * 启动服务
+ * @param cls 服务类
+ */
+fun startService(cls: Class<*>) = run {
+    appContext.startService(Intent(appContext, cls))
+}
+
+/**
+ * 启动服务
+ * @param className 完整包名的服务类名
+ */
+fun startService(className: String) = runCatching {
+    startService(Class.forName(className))
+}.onFailure {
+    it.printStackTrace()
+}
+
+/**
+ * 停止服务
+ * @param cls 服务类
+ * @return true：停止成功  false：停止失败
+ */
+fun stopService(cls: Class<*>): Boolean = run {
+    appContext.stopService(Intent(appContext, cls))
+}
+
+/**
+ * 停止服务
+ * @param className 完整包名的服务类名
+ * @return true：停止成功  false：停止失败
+ */
+fun stopService(className: String): Boolean = runCatching {
+    stopService(Class.forName(className))
+}.onFailure {
+    it.printStackTrace()
+}.getOrDefault(false)
+
+/**
+ * 绑定服务
+ * @param cls   服务类
+ * @param conn  服务连接对象
+ * @param flags 绑定选项
+ * [Context.BIND_AUTO_CREATE]
+ * [Context.BIND_DEBUG_UNBIND]
+ * [Context.BIND_NOT_FOREGROUND]
+ * [Context.BIND_ABOVE_CLIENT]
+ * [Context.BIND_ALLOW_OOM_MANAGEMENT]
+ * [Context.BIND_WAIVE_PRIORITY]
+ */
+fun bindService(cls: Class<*>, conn: ServiceConnection, flags: Int) = run {
+    appContext.bindService(Intent(appContext, cls), conn, flags)
+}
+
+/**
+ * 绑定服务
+ * @param className 完整包名的服务类名
+ * @param conn      服务连接对象
+ * @param flags     绑定选项
+ */
+fun bindService(className: String, conn: ServiceConnection, flags: Int) = runCatching {
+    bindService(Class.forName(className), conn, flags)
+}.onFailure {
+    it.printStackTrace()
+}
+
+/**
+ * 解绑服务
+ * @param conn 服务连接对象
+ */
+fun unBindService(conn: ServiceConnection) = run {
+    appContext.unbindService(conn)
+}
+
+/**
+ * 判断服务是否运行
+ * @param cls    服务类
+ * @return true：是   false：否
+ */
+@Suppress("DEPRECATION")
+fun isServiceRunning(cls: Class<*>): Boolean = run {
+    appContext.activityManager?.getRunningServices(Integer.MAX_VALUE)
+        ?.any { it -> it.service.className == cls.name }
+        ?: false
+}
+
+/**
+ * 判断服务是否运行
+ * @param className 完整包名的服务类名
+ * @return true：是   false：否
+ */
+fun isServiceRunning(className: String): Boolean = runCatching {
+    isServiceRunning(Class.forName(className))
+}.onFailure {
+    it.printStackTrace()
+}.getOrDefault(false)
