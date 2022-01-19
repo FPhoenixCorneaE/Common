098dec7537ccc93057cc80297ac37c4e60f14f95
wangkz <wangkz@infore.com>
2022-01-19 16:14:10 +0800

    perf:AudioKt.kt;

--- common/src/main/kotlin/com/fphoenixcorneae/common/ext/AudioKt.kt
+++ common/src/main/kotlin/com/fphoenixcorneae/common/ext/AudioKt.kt
@@ -1,15 +1,19 @@
 package com.fphoenixcorneae.common.ext
 
 import android.media.AudioManager
+import android.os.Build
 
 /**
  * 获取当前媒体音量
- * * [AudioManager.STREAM_SYSTEM]       系统声音
- * * [AudioManager.STREAM_VOICE_CALL]   通话声音
- * * [AudioManager.STREAM_RING]         铃声声音
- * * [AudioManager.STREAM_MUSIC]        媒体声音
- * * [AudioManager.STREAM_ALARM]        提示声音
- * * [AudioManager.STREAM_NOTIFICATION] 通知声音
+ * @param streamType The stream type.
+ * * [AudioManager.STREAM_SYSTEM]        系统声音
+ * * [AudioManager.STREAM_VOICE_CALL]    通话声音
+ * * [AudioManager.STREAM_RING]          铃声声音
+ * * [AudioManager.STREAM_MUSIC]         媒体声音
+ * * [AudioManager.STREAM_ALARM]         提示声音
+ * * [AudioManager.STREAM_NOTIFICATION]  通知声音
+ * * [AudioManager.STREAM_DTMF]          双音多频
+ * * [AudioManager.STREAM_ACCESSIBILITY] 无障碍性
  */
 fun getStreamVolume(
     streamType: Int = AudioManager.STREAM_MUSIC
@@ -17,13 +21,33 @@ fun getStreamVolume(
     appContext.audioManager?.getStreamVolume(streamType) ?: 0
 
 /**
+ * 设置音量
+ * @param volumeValue The volume.
+ * @param flags       The flags.
+ * * [AudioManager.FLAG_SHOW_UI]
+ * * [AudioManager.FLAG_ALLOW_RINGER_MODES]
+ * * [AudioManager.FLAG_PLAY_SOUND]
+ * * [AudioManager.FLAG_REMOVE_SOUND_AND_VIBRATE]
+ * * [AudioManager.FLAG_VIBRATE]
+ */
+fun setStreamVolume(
+    volumeValue: Int,
+    streamType: Int = AudioManager.STREAM_MUSIC,
+    flags: Int = 0
+) =
+    appContext.audioManager?.setStreamVolume(streamType, volumeValue, flags)
+
+/**
  * 获取最大音量
- * * [AudioManager.STREAM_SYSTEM]       系统声音
- * * [AudioManager.STREAM_VOICE_CALL]   通话声音
- * * [AudioManager.STREAM_RING]         铃声声音
- * * [AudioManager.STREAM_MUSIC]        媒体声音
- * * [AudioManager.STREAM_ALARM]        提示声音
- * * [AudioManager.STREAM_NOTIFICATION] 通知声音
+ * @param streamType The stream type.
+ * * [AudioManager.STREAM_SYSTEM]        系统声音
+ * * [AudioManager.STREAM_VOICE_CALL]    通话声音
+ * * [AudioManager.STREAM_RING]          铃声声音
+ * * [AudioManager.STREAM_MUSIC]         媒体声音
+ * * [AudioManager.STREAM_ALARM]         提示声音
+ * * [AudioManager.STREAM_NOTIFICATION]  通知声音
+ * * [AudioManager.STREAM_DTMF]          双音多频
+ * * [AudioManager.STREAM_ACCESSIBILITY] 无障碍性
  */
 fun getStreamMaxVolume(
     streamType: Int = AudioManager.STREAM_MUSIC
@@ -31,10 +55,22 @@ fun getStreamMaxVolume(
     appContext.audioManager?.getStreamMaxVolume(streamType) ?: 0
 
 /**
- * 设置音量
+ * 获取最小音量
+ * @param streamType The stream type.
+ * * [AudioManager.STREAM_SYSTEM]        系统声音
+ * * [AudioManager.STREAM_VOICE_CALL]    通话声音
+ * * [AudioManager.STREAM_RING]          铃声声音
+ * * [AudioManager.STREAM_MUSIC]         媒体声音
+ * * [AudioManager.STREAM_ALARM]         提示声音
+ * * [AudioManager.STREAM_NOTIFICATION]  通知声音
+ * * [AudioManager.STREAM_DTMF]          双音多频
+ * * [AudioManager.STREAM_ACCESSIBILITY] 无障碍性
  */
-fun setStreamVolume(
-    volumeValue: Int,
+fun getStreamMinVolume(
     streamType: Int = AudioManager.STREAM_MUSIC
-) =
-    appContext.audioManager?.setStreamVolume(streamType, volumeValue, 0)
+): Int =
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+        appContext.audioManager?.getStreamMinVolume(streamType) ?: 0
+    } else {
+        0
+    }
