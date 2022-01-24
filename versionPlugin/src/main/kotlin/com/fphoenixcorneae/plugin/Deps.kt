5af77fa89ca4da22154701fff45c229bc8da4604
wangkz <wangkz@infore.com>
2022-01-24 10:28:46 +0800

    build: component version upgrade to "2.0.0";

--- versionPlugin/src/main/kotlin/com/fphoenixcorneae/plugin/Deps.kt
+++ versionPlugin/src/main/kotlin/com/fphoenixcorneae/plugin/Deps.kt
@@ -7,8 +7,8 @@ object Deps {
         const val buildToolsVersion = "31.0.0"
         const val minSdkVersion = 21
         const val targetSdkVersion = 31
-        const val versionCode = 118
-        const val versionName = "1.1.8"
+        const val versionCode = 200
+        const val versionName = "2.0.0"
     }
 
     /** BuildType */
