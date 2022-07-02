97a68d30228c911a0615d70ecc3f05ef6ac68d02
飄然偌僊 <13104871646@163.com>
2022-07-02 21:14:52 +0800

    fix:ColorKt：设置颜色透明度; build: component version upgrade to 2.0.9;

--- versionPlugin/src/main/kotlin/com/fphoenixcorneae/plugin/Deps.kt
+++ versionPlugin/src/main/kotlin/com/fphoenixcorneae/plugin/Deps.kt
@@ -7,8 +7,8 @@ object Deps {
         const val buildToolsVersion = "31.0.0"
         const val minSdkVersion = 21
         const val targetSdkVersion = 31
-        const val versionCode = 208
-        const val versionName = "2.0.8"
+        const val versionCode = 209
+        const val versionName = "2.0.9"
     }
 
     /** BuildType */
